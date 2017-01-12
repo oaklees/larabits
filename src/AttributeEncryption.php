86feb8f6f7b607a6c7791cde2b1430a0cd5ea0ba
Andrew Lees <andrew.lees@moto-way.co.uk>
2017-01-12 16:55:34 +0000

    Initial commit

---
+++ src/AttributeEncryption.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Larabits\Eloquent;
+
+trait AttributeEncryption
+{
+	/**
+	 * Called when attribute accessed by model.
+	 *
+	 * @param $attribute
+	 * @return string
+	 */
+	public function getAttribute($attribute)
+	{
+		if ($this->shouldEncrypt($attribute)) {
+			// As this attribute is encrypted, return decrypted version
+			return decrypt($this->getAttributeValue($attribute));
+		} else {
+			// Let Laravel handle the request.
+			return parent::getAttribute($attribute);
+		}
+	}
+
+	/**
+	 * Called when attribute being written to by model.
+	 *
+	 * @param $attribute
+	 * @param $value
+	 * @return mixed
+	 */
+	public function setAttribute($attribute, $value)
+	{
+		if ($this->shouldEncrypt($attribute)) {
+			// As this attribute is encrypted, write encrypted version
+			$this->attributes[$attribute] = encrypt($value);
+		} else {
+			// Let Laravel handle the request.
+			return parent::setAttribute($attribute,$value);
+		}
+	}
+
+	/**
+	 * Determine whether given attribute should be encrypted.
+	 *
+	 * @param $attribute
+	 * @return bool
+	 */
+	private function shouldEncrypt($attribute)
+	{
+		return in_array($attribute,$this->encrypt,true) && env('APP_ENABLE_ENCRYPTION',true);
+	}
+}
